e4f0df182d913ccec871083ef585212afa41ccd6
williandamasw <willian.7839502@aluno.mg.gov.br>
2026-04-10 10:34:52 -0300

    Update and rename README.md to Principal.java

---
+++ Principal.java
@@ -0,0 +1,31 @@
+package [Willian];
+public class Principal {
+    public static void main(String[] args) {
+
+        Individuo p1 = new 
+
+Individuo  ();
+        p1.nome = "Ana";
+        p1.idade = 20;
+        p1.caminhar();
+        p1.dormir();
+
+        
+
+Individuo   p2 = new 
+
+Individuo  ();
+        p2.nome = "Bruno";
+        p2.idade = 30;
+
+        
+
+Individuo   p3 = new 
+
+Individuo  ();
+        p3.nome = "Carla";
+        p3.idade = 25;
+        p3.caminhar();
+        p3.dormir();
+    }
+}
