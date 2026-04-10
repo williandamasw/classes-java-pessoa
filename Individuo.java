168af6e754ecfea0811c08af057268741490f838
williandamasw <willian.7839502@aluno.mg.gov.br>
2026-04-10 10:31:14 -0300

    Create Individuo.java

---
+++ Individuo.java
@@ -0,0 +1,12 @@
+ackage [Willian];
+
+public class Pessoa {
+String nome;
+int idade;
+        public void caminhar() {
+        System.out.println("caminhando...");
+        }
+        public void dormir() {
+        System.out.println("dormir...");
+        }
+}
